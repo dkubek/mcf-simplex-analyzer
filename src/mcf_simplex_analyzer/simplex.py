06794a2efc14f815b529143160522cf4885cc5c4
David Kubek <david.kubek@gmail.com>
2021-08-11 19:38:14 +0200

    Refactor simplex output

--- src/mcf_simplex_analyzer/simplex.py
+++ src/mcf_simplex_analyzer/simplex.py
@@ -1,3 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+The simplex algorithm.
+
+TODO:
+    - Refactor output
+    - Refactor instantiation
+    - Two phase simplex
+    - Add tests
+
+"""
+
+import logging
 from enum import Enum
 
 import attr
@@ -5,25 +18,7 @@ import numpy as np
 
 from .fractionarray import FractionArray
 
-DEFAULT_MAX_ITERATIONS = 1e2
-
-
-def dantzig_entering(objective, base):
-    nonbasic = np.where(~base)[0]
-    positive = np.where(objective[nonbasic] > 0)[0]
-    entering = nonbasic[positive[objective[nonbasic][positive].argmax()]]
-
-    return entering
-
-
-def dantzig_leaving(table, entering):
-    positive = np.where(table[..., entering] > 0)[0]
-
-    bounds = table[..., -1][positive] / table[..., entering][positive]
-    valid = np.where(bounds >= 0)[0]
-    leaving_row = positive[valid[bounds[valid].argmin()]]
-
-    return leaving_row
+DEFAULT_MAX_ITERATIONS = 1e5
 
 
 class LPFormType(Enum):
@@ -42,18 +37,40 @@ class LPFormulation:
     meta: dict = attr.ib(kw_only=True, factory=dict)
 
 
+def _validate_formulation(formulation):
+    if len(np.shape(formulation.table)) != 2:
+        raise ValueError(
+            "Invalid table shape: {}".format(np.shape(formulation.table))
+        )
+
+    if len(np.shape(formulation.rhs)) != 1:
+        raise ValueError(
+            "Invalid right hand side shape: {}".format(
+                np.shape(formulation.rhs)
+            )
+        )
+
+    if len(np.shape(formulation.objective)) != 1:
+        raise ValueError(
+            "Invalid objective function shape: {}".format(
+                np.shape(formulation.objective)
+            )
+        )
+
+
 @attr.s(kw_only=True)
 class Simplex:
     table = attr.ib()
-    objective = attr.ib()
+    _objective_row = attr.ib()
 
-    _base = attr.ib()
+    base = attr.ib()
     _row_to_var_index = attr.ib()
     _var_index_to_row = attr.ib()
 
     @classmethod
     def _from_canonical(cls, form: LPFormulation):
         m, n = form.table.shape
+
         if np.all(form.rhs >= 0):
             slack = np.eye(m, dtype=form.table.numerator.dtype)
             new_table = FractionArray(
@@ -99,7 +116,7 @@ class Simplex:
 
             return cls(
                 table=new_table,
-                objective=new_objective,
+                objective_row=new_objective,
                 base=base,
                 row_to_var_index=row_to_var_index,
                 var_index_to_row=var_index_to_row,
@@ -108,20 +125,134 @@ class Simplex:
             raise NotImplementedError("Two phase simplex not implemented.")
 
     @classmethod
-    def from_formulation(cls, formulation: LPFormulation):
+    def instantiate(cls, formulation: LPFormulation):
+        """
+        Instantiate the simplex algorithm for the given problem formulation.
+
+        Args:
+            formulation (LPFormulation): The LP formulation
+
+        Raises:
+            ValueError: If the input formulation is invalid.
+
+        Returns:
+            (Simplex): An instance of the simplex algorithm for the given
+            problem.
+
+        """
+
+        _validate_formulation(formulation)
+
         if formulation.type == LPFormType.Canonical:
             return cls._from_canonical(formulation)
         else:
             raise NotImplementedError
 
+    def state(self):
+        """ Return the value of the objective function and variable values. """
+
+        variables = FractionArray.from_array(
+            np.zeros(self.table.shape[1], dtype=int)
+        )
+        variables[self._row_to_var_index] = self.table[
+            self._var_index_to_row[self._row_to_var_index], -1
+        ]
+
+        return {"value": self.objective_val, "variables": variables}
+
+    def solve(
+        self,
+        decision_rule="dantzig",
+        degenerate_max_iter=DEFAULT_MAX_ITERATIONS,
+    ):
+        """
+        Start the simplex algorithm.
+
+        Args:
+            decision_rule:
+            degenerate_max_iter:
+
+        Returns:
+
+        """
+
+        logger = logging.getLogger(__name__)
+        logger.info("Starting the simplex algorithm.")
+        logger.debug(
+            "decision_rule=%s, degenerate_max_iter",
+            decision_rule,
+            degenerate_max_iter,
+        )
+
+        if decision_rule not in DECISION_RULES:
+            raise ValueError(
+                "Decision rule {} is invalid or "
+                "unsupported".format(decision_rule)
+            )
+
+        decision_function = DECISION_RULES[decision_rule]
+
+        last_objective = self.objective_val
+        iterations_from_last_increase = 0
+
+        iteration_count = 0
+        while True:
+            logger.debug("Iteration count %d", iteration_count)
+
+            # Check whether the optimal value has been reached
+            if self._is_end():
+                logger.info("Success")
+                return {"result": "success", **self.state()}
+
+            # Determine the entering variable
+            entering = decision_function(self)
+            logger.debug("Entering:", entering)
+
+            if self._is_unbounded(entering):
+                logger.info("The problem is unbounded.")
+                return {"result": "unbounded"}
+
+            leaving_row = self._determine_leaving_row(entering)
+            logger.debug("Leaving row: %d", leaving_row)
+
+            # Update the table
+            update_row = self._get_update_row(entering, leaving_row)
+
+            self._update_table(update_row, entering, leaving_row)
+            self._update_objective(update_row, entering)
+            self._update_base(entering, leaving_row)
+
+            if self.objective_val > last_objective:
+                last_objective = self.objective_val
+                iterations_from_last_increase = 0
+                continue
+
+            iterations_from_last_increase += 1
+            if iterations_from_last_increase >= degenerate_max_iter:
+                logger.info("Max iterations reached, probably cycling.")
+                return {"result": "cycle"}
+
+            iteration_count += 1
+
+    def _determine_leaving_row(self, entering):
+        positive = np.where(self.table[..., entering] > 0)[0]
+
+        bounds = (
+            self.table[..., -1][positive] / self.table[..., entering][positive]
+        )
+        valid = np.where(bounds >= 0)[0]
+        leaving_row = positive[valid[bounds[valid].argmin()]]
+
+        return leaving_row
+
     def _get_update_row(self, entering, leaving_row):
         return self.table[leaving_row] / self.table[leaving_row, entering]
 
     def _update_objective(self, update_row, entering):
-        update = update_row * self.objective[entering]
+        update = update_row * self._objective_row[entering]
         update[:-1] *= -1
 
-        self.objective += update
+        self._objective_row += update
 
     def _update_base(self, entering, leaving_row):
         leaving = self._row_to_var_index[leaving_row]
@@ -129,8 +260,8 @@ class Simplex:
 
         self._var_index_to_row[entering] = self._var_index_to_row[leaving]
 
-        self._base[entering] = True
-        self._base[leaving] = False
+        self.base[entering] = True
+        self.base[leaving] = False
 
     def _update_table(self, update_row, entering, leaving_row):
         self.table[leaving_row] = update_row
@@ -142,49 +273,31 @@ class Simplex:
             self.table[row] -= self.table[row, entering] * update_row
 
     def _is_end(self):
-        return np.all(self.objective[:-1][~self._base] < 0)
+        return np.all(self.objective_fun[~self.base] < 0)
 
-    def solve(self):
-        last_objective = self.objective[-1]
-        iterations_from_last_increase = 0
+    def _is_unbounded(self, entering):
+        return np.all(self.table[..., entering] < 0)
 
-        while True:
-            print(self.objective)
-            print("Objective function:", self.objective)
-            for var in self._row_to_var_index:
-                print(f"x_{var}:", self.table[self._var_index_to_row[var], -1])
+    def __getattr__(self, attribute):
+        if attribute == "objective_val":
+            return self._objective_row[-1]
+        elif attribute == "objective_fun":
+            return self._objective_row[:-1]
+        else:
+            raise AttributeError(attribute + " not found")
 
-            # Check end
-            if self._is_end():
-                print("End")
-                break
 
-            # Determine entering
-            entering = dantzig_entering(self.objective[:-1], self._base)
-            print("Entering:", entering)
+def dantzig(simplex: Simplex):
+    nonbasic = np.where(~simplex.base)[0]
+    positive = np.where(simplex.objective_fun[nonbasic] > 0)[0]
+    entering = nonbasic[
+        positive[simplex.objective_fun[nonbasic][positive].argmax()]
+    ]
 
-            # Check unbounded
-            if np.all(self.table[..., entering] < 0):
-                print("Unbounded")
-                break
+    return entering
 
-            # Determine leaving
-            leaving_row = dantzig_leaving(self.table, entering)
-            print("Leaving row:", leaving_row)
+def bland(simplex: Simplex):
+    pass
 
-            # Update
-            update_row = self._get_update_row(entering, leaving_row)
 
-            self._update_table(update_row, entering, leaving_row)
-            self._update_objective(update_row, entering)
-            self._update_base(entering, leaving_row)
-
-            if self.objective[-1] > last_objective:
-                last_objective = self.objective[-1]
-                iterations_from_last_increase = 0
-                continue
-
-            iterations_from_last_increase += 1
-            if iterations_from_last_increase >= DEFAULT_MAX_ITERATIONS:
-                print("Max iterations reached, probably cycling")
-                break
+DECISION_RULES = {"dantzig": dantzig}
